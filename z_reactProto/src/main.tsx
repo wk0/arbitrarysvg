6dc5734da7a868079b1a0e823ad39d6d5c3653f5
Will  Kelly <willkelly0@gmail.com>
2022-08-06 18:12:30 -0400

    wagmi config

--- z_reactProto/src/main.tsx
+++ z_reactProto/src/main.tsx
@@ -3,8 +3,41 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 
+import { WagmiConfig, createClient, chain, configureChains } from "wagmi"
+
+import { InjectedConnector } from "wagmi/connectors/injected"
+import { publicProvider } from "wagmi/providers/public"
+
+const { chains, provider } = configureChains(
+    [
+        chain.mainnet,
+        chain.rinkeby,
+        chain.polygon,
+        chain.polygonMumbai,
+        chain.localhost,
+        chain.foundry,
+    ],
+    [publicProvider()]
+)
+
+const client = createClient({
+    autoConnect: true,
+    connectors: [
+        new InjectedConnector({
+            chains,
+            options: {
+                name: "Injected",
+                shimDisconnect: true,
+            },
+        }),
+    ],
+    provider,
+})
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <WagmiConfig client={client}>
+            <App />
+        </WagmiConfig>
     </React.StrictMode>
 )
