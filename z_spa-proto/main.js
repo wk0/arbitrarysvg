1ebef7124cab2c9f006ed76903364684c00ed832
Will  Kelly <willkelly0@gmail.com>
2022-08-05 22:02:10 -0400

    add app

--- z_spa-proto/main.js
+++ z_spa-proto/main.js
@@ -1,23 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <h1>Test</h1>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
