fe585971b89ef6524c0618d908311ac969b1f98a
Will  Kelly <willkelly0@gmail.com>
2022-08-05 23:48:06 -0400

    init react spa

---
+++ z_reactProto/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react"
+
+const contractAddress = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9"
+const tokenId = "1"
+// RPC url to access blockchain with. Optional: will fallback to using cloudflare eth
+
+function App() {
+    return (
+        <div className="App">
+            <h1>ZoraNFT Hack</h1>
+        </div>
+    )
+}
+
+export default App
