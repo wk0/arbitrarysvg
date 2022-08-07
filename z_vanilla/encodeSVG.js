14d0637bbf0b7b7a209043881cb4c48a156b14c5
Will  Kelly <willkelly0@gmail.com>
2022-08-07 00:59:21 -0400

    output image and token encoded

--- z_vanilla/encodeSVG.js
+++ z_vanilla/encodeSVG.js
@@ -13,6 +13,8 @@ const buff = fs.readFileSync('./public/test3.svg');
 const base64data = buff.toString('base64');
 const mime = "data:image/svg+xml;base64,";
 
+fs.writeFileSync('./public/encodedSVG', `${mime}${base64data}`);
+
 // create json tokenURI string
 const json = `{"name": "NFT #1", "description": "Test", "image_data": "${mime}${base64data}"}`;
 
@@ -22,4 +24,4 @@ const encoded = jsonBuffer.toString('base64');
 const outputMime = 'data:application/json;base64,'
 
 // write out simulated tokenURI
-fs.writeFileSync('./public/encodedTestSVG', `${outputMime}${encoded}`);
+fs.writeFileSync('./public/encodedTokenURI', `${outputMime}${encoded}`);
