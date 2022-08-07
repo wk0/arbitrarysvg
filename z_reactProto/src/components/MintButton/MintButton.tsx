4e208349fda62b5a696ddb2be72f9d85fb5cf904
Will  Kelly <willkelly0@gmail.com>
2022-08-07 02:12:48 -0400

    mint button

---
+++ z_reactProto/src/components/MintButton/MintButton.tsx
@@ -0,0 +1,36 @@
+import { useMint } from "../../hooks/useContract"
+import { BigNumber } from "ethers"
+import { useEffect } from "react"
+
+interface MintButton {
+    address: string
+    setTokenId: React.Dispatch<React.SetStateAction<BigNumber | null>>
+}
+
+export const MintButton = ({ address, setTokenId }: MintButton) => {
+    const { error, write, mintError, tokenId } = useMint(address)
+
+    useEffect(() => {
+        if (tokenId) {
+            setTokenId(tokenId)
+        }
+    }, [tokenId])
+
+    return (
+        <div>
+            <button disabled={!write} onClick={() => write?.()}>
+                Mint
+            </button>
+            {error && (
+                <div>
+                    An error occurred preparing the transaction: {error.message}
+                </div>
+            )}
+            {mintError && (
+                <div>
+                    An error occurred preparing the transaction: {mintError}
+                </div>
+            )}
+        </div>
+    )
+}
---
+++ z_reactProto/src/components/MintButton/index.ts
@@ -0,0 +1,1 @@
+export { MintButton } from './MintButton'
