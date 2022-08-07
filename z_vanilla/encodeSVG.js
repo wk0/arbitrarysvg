72a204d1cc1ef5fc58c00c78105822fdc6f80aee
Will  Kelly <willkelly0@gmail.com>
2022-08-06 22:51:56 -0400

    can make interactive svg

--- z_vanilla/encodeSVG.js
+++ z_vanilla/encodeSVG.js
@@ -1,8 +1,17 @@
 import * as fs from 'fs';
 
+// read SVG and encode
 const buff = fs.readFileSync('./public/test.svg');
 const base64data = buff.toString('base64');
-
 const mime = "data:image/svg+xml;base64,";
 
-fs.writeFileSync('./public/encodedTestSVG', `${mime}${base64data}`);
+// create json tokenURI string
+const json = `{"name": "NFT #1", "description": "Test", "image_data": "${mime}${base64data}"}`;
+
+// encode json
+const jsonBuffer = Buffer.from(json);
+const encoded = jsonBuffer.toString('base64');
+const outputMime = 'data:application/json;base64,'
+
+// write out simulated tokenURI
+fs.writeFileSync('./public/encodedTestSVG', `${outputMime}${encoded}`);
